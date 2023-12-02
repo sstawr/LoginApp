8c3c8283c7123222ed6e0bd91a6ef45888db5d31
Evgeni Glushko <sstawr@gmail.com>
2023-12-02 19:56:05 +0300

    Work with data

---
+++ LoginApp/Models/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  LoginApp
+//
+//  Created by Evgeni Glushko on 2.12.23.
+//
+
+import Foundation
---
+++ LoginApp/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  LoginApp
+//
+//  Created by Evgeni Glushko on 2.12.23.
+//
+
+import Foundation
